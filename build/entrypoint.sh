c08ac95b20e2a6a0ef35ff08ba01fd2f5394cf8c
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2018-10-04 01:35:08 -0700

    Set content type header

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "curl -H 'Authorization: $GITHUB_TOKEN' -H 'X-GitHub-Event: $GITHUB_EVENT' --data-binary '@$GITHUB_EVENT_PATH' https://api.netlify.com/api/v1/github/$GITHUB_REPOSITORY/build"
+sh -c "curl -H 'Authorization: $GITHUB_TOKEN' -H 'X-GitHub-Event: $GITHUB_EVENT' -H 'Content-Type: application/json' --data-binary '@$GITHUB_EVENT_PATH' https://api.netlify.com/api/v1/github/$GITHUB_REPOSITORY/build"
